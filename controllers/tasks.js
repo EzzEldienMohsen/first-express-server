0b0c11e42110c5c2eafc145fe5cbe8f13e40da55
Ezz dghedy <ezzmohsend@gmail.com>
2025-01-25 00:02:07 +0200

    async wrapper

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,65 +1,43 @@
 const pool = require('../db');
+const asyncWrapper = require('../middleware/async');
 
-const getAllTasks = async (req, res) => {
-  try {
-    const result = await pool.query('SELECT * FROM task');
-    console.log(result);
-    res.status(200).json({ tasks: result.rows });
-  } catch (err) {
-    console.error('Database query failed', err);
-    res.status(500).json({ error: 'Failed to fetch tasks' });
-  }
-};
-const getTask = async (req, res) => {
+const getAllTasks = asyncWrapper(async (req, res) => {
+  const result = await pool.query('SELECT * FROM task');
+  console.log(result);
+  res.status(200).json({ tasks: result.rows });
+});
+const getTask = asyncWrapper(async (req, res) => {
   const { id } = req.params;
-  try {
-    const result = await pool.query('SELECT * FROM task WHERE id = $1', [id]);
-    res.status(200).json({ task: result.rows });
-  } catch (err) {
-    console.error('Database query failed', err);
-    res.status(500).json({ error: 'Failed to fetch tasks' });
-  }
-};
-const updateTask = async (req, res) => {
+  const result = await pool.query('SELECT * FROM task WHERE id = $1', [id]);
+  res.status(200).json({ task: result.rows });
+});
+const updateTask = asyncWrapper(async (req, res) => {
   const { id } = req.params;
   const { name, completed } = req.body;
-  try {
-    const result = await pool.query(
-      'UPDATE task SET name = $1, completed =$2 WHERE id = $3 RETURNING *',
-      [name, completed, id]
-    );
-    res.status(201).json({ task: result.rows });
-  } catch (err) {
-    console.error('Database query failed', err);
-    res.status(500).json({ error: 'Failed to fetch tasks' });
-  }
-};
-const deleteTask = async (req, res) => {
+
+  const result = await pool.query(
+    'UPDATE task SET name = $1, completed =$2 WHERE id = $3 RETURNING *',
+    [name, completed, id]
+  );
+  res.status(201).json({ task: result.rows });
+});
+
+const deleteTask = asyncWrapper(async (req, res) => {
   const { id } = req.params;
 
-  try {
-    const result = await pool.query(
-      'DELETE FROM task WHERE id = $1 RETURNING *',
-      [id]
-    );
-    res.status(204).json({ task: result.rows });
-  } catch (err) {
-    console.error('Database query failed', err);
-    res.status(500).json({ error: 'Failed to fetch tasks' });
-  }
-};
-const addTask = async (req, res) => {
+  const result = await pool.query(
+    'DELETE FROM task WHERE id = $1 RETURNING *',
+    [id]
+  );
+  res.status(204).json({ task: result.rows });
+});
+const addTask = asyncWrapper(async (req, res) => {
   const { name, completed = false } = req.body; // Default false
-  try {
-    const result = await pool.query(
-      'INSERT INTO task (name, completed) VALUES ($1, $2) RETURNING *',
-      [name, completed]
-    );
-    res.status(201).json({ task: result.rows[0] }); // Return single task object
-  } catch (err) {
-    console.error('Database query failed', err);
-    res.status(500).json({ error: 'Failed to add task' });
-  }
-};
+  const result = await pool.query(
+    'INSERT INTO task (name, completed) VALUES ($1, $2) RETURNING *',
+    [name, completed]
+  );
+  res.status(201).json({ task: result.rows[0] }); // Return single task object
+});
 
 module.exports = { getAllTasks, addTask, getTask, updateTask, deleteTask };
